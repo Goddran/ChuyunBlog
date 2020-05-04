3f3fbf019622f1b6198ef02f91132e18c9a83bf2
saysky <847064370@qq.com>
2020-05-04 12:34:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# 指定项目的根目录，请按实际修改
+# 指定SENS的根目录，请按实际修改
 SENS_DIR="/www/wwwroot/ChuyunBlog"
 # 拉取最新的源码
 git pull
 echo "代码拉取完毕！"
 
-# 停止项目
+# 进入SENS根目录
+cd $SENS_DIR
+
+# 停止SENS
 sh $SENS_DIR/bin/sens.sh stop
 
 # 执行打包
 mvn clean package -Dmaven.test.skip=true
-echo "项目打包完成！"
+echo "编译打包完毕！"
 
-# 运行SENS
+# 进入打包好的target目录
+cd $SENS_DIR/target
+
+# 运行项目
 sh $SENS_DIR/bin/sens.sh start
 
 echo "部署完毕，Enjoy！"
